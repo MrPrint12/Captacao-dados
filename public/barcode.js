a6bd280a9bb5f26340f401a87f66fdba52b49183
MrPrint12 <ninamariomarioo@gmail.com>
2025-05-28 12:55:03 +0200

    Add files via upload

---
+++ public/barcode.js
@@ -0,0 +1,5 @@
+
+// JsBarcode CDN loader (or add your own build if needed)
+const script = document.createElement('script');
+script.src = "https://cdn.jsdelivr.net/npm/jsbarcode@3.11.5/dist/JsBarcode.all.min.js";
+document.head.appendChild(script);
